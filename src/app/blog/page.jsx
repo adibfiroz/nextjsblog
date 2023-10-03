32c6631f721d62c1b07ed46758e80750c3e5aa88
adibfiroz <adibfiroz9702@gmail.com>
2023-10-03 11:21:07 +0530

    changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CardList from "@/components/cardList/CardList";
 import styles from "./blog.module.css";
-import Menu from "@/components/Menu/Menu";
+import Menu from "@/components/menu/Menu";
 
 const BlogPage = ({ searchParams }) => {
   const page = parseInt(searchParams.page) || 1;
